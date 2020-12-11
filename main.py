0139d71e4a54035011bda14d69df6c68a1c856c3
eduardovbe <51214452+eduardovbe@users.noreply.github.com>
2020-12-11 19:39:22 -0300

    Opção AFN OU AFD

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
+from Sources.AFN import *
 from Sources.Classes import *
+
+
 
 if __name__ == "__main__":
     Nome_Arq = str(input("Entre com o nome do arquivo a ser aberto:"))
-    Arq = open("Arq_Entrada/"+Nome_Arq, 'r')
+    Arq = open("Arq_Entrada/" + Nome_Arq, 'r')
     # Arq=open("Arq_Entrada/Entrada1.txt",'r')
-    Aux=0
+    Aux = 0
+    Aux2 = 0
     print("Menu:")
     print("\t1 - Alfabeto {0, 1}")
     print("\t2 - Alfabeto Arbitrario")
-    while(Aux!=1 and Aux!=2):
-        Aux=int(input("Entre com uma opcao:"))
-        if(Aux != 1 and Aux != 2):
+    while (Aux != 1 and Aux != 2):
+        Aux = int(input("Entre com uma opcao:"))
+        if (Aux != 1 and Aux != 2):
             print("\nERRO!Opcao invalida!\n")
-    if(Aux==1):
+    print("\t1 - AFD")
+    print("\t2 - AFN")
+    while (Aux2 != 1 and Aux2 != 2):
+        Aux2 = int(input("Entre com uma opcao:"))
+        if (Aux2 != 1 and Aux2 != 2):
+            print("\nERRO!Opcao invalida!\n")
+    if (Aux == 1):
         Q = Arq.readline()
         I = Arq.readline()
         F = Arq.readline()
-
-        Automato1 = Automato(Q, I, F)  #Instanciando objeto com respectivos estados
+        Automato1 = Automato(Q, I, F)
+        if Aux2 == 1:
+            Automato1 = Automato(Q, I, F)  # Instanciando objeto com respectivos estados
+        elif Aux2 == 2:
+            Automato1 = AutomatoAFN(Q, I, F)  # Instanciando objeto com respectivos estados
 
         for linha in Arq:
-            if(linha=="---\n"):
+            if (linha == "---\n"):
                 break
-            Automato1.AdicionaTransicao(linha) #Adicionando transicoes
+            Automato1.AdicionaTransicao(linha)  # Adicionando transicoes
 
         for linha in Arq:
-            Automato1.AdicionaEntrada(linha) #Adicionando Entradas
+            Automato1.AdicionaEntrada(linha)  # Adicionando Entradas
 
-        print(Automato1.Estados)  #Prints de debug
+        print(Automato1.Estados)  # Prints de debug
         print(Automato1.EstadoI)
         print(Automato1.EstadosF)
         print(Automato1.Origem)
@@ -37,7 +50,7 @@ if __name__ == "__main__":
 
         Automato1.RealizaComputacao()
 
-    elif(Aux==2):
+    elif (Aux == 2):
         Q = Arq.readline()
         S = Arq.readline()
         I = Arq.readline()
@@ -47,7 +60,7 @@ if __name__ == "__main__":
         Automato1 = Automato(Q, I, F)
 
         for linha in Arq:
-            if(linha == "---\n"):
+            if (linha == "---\n"):
                 break
             Automato1.AdicionaTransicao(linha)  # Adicionando transicoes
 
