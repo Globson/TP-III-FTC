b27259bd115993bb8265c1b537044b62027a0faa
eduardovbe <51214452+eduardovbe@users.noreply.github.com>
2020-12-12 19:34:16 -0300

    Verificação de \ na entrada

--- main.py
+++ main.py
@@ -2,81 +2,88 @@ from Sources.AFN import *
 from Sources.Classes import *
 
 
+def main():
+    if __name__ == "__main__":
+        Nome_Arq = str(input("Entre com o nome do arquivo a ser aberto:"))
+        Arq = open("Arq_Entrada/" + Nome_Arq, 'r')
+        # Arq=open("Arq_Entrada/Entrada1.txt",'r')
+        Aux = 0
+        Aux2 = 0
+        print("Menu:")
+        print("\t1 - Alfabeto {0, 1}")
+        print("\t2 - Alfabeto Arbitrario")
+        while (Aux != 1 and Aux != 2):
+            Aux = int(input("Entre com uma opcao:"))
+            if (Aux != 1 and Aux != 2):
+                print("\nERRO!Opcao invalida!\n")
+        print("\t1 - AFD")
+        print("\t2 - AFN")
+        while (Aux2 != 1 and Aux2 != 2):
+            Aux2 = int(input("Entre com uma opcao:"))
+            if (Aux2 != 1 and Aux2 != 2):
+                print("\nERRO!Opcao invalida!\n")
+        if (Aux == 1):
+            Q = Arq.readline()
+            I = Arq.readline()
+            F = Arq.readline()
+            if Aux2 == 1:
+                Automato1 = Automato(Q, I, F)  # Instanciando objeto com respectivos estados
+            elif Aux2 == 2:
+                Automato1 = AutomatoAFN(Q, I, F)  # Instanciando objeto com respectivos estados
 
-if __name__ == "__main__":
-    Nome_Arq = str(input("Entre com o nome do arquivo a ser aberto:"))
-    Arq = open("Arq_Entrada/" + Nome_Arq, 'r')
-    # Arq=open("Arq_Entrada/Entrada1.txt",'r')
-    Aux = 0
-    Aux2 = 0
-    print("Menu:")
-    print("\t1 - Alfabeto {0, 1}")
-    print("\t2 - Alfabeto Arbitrario")
-    while (Aux != 1 and Aux != 2):
-        Aux = int(input("Entre com uma opcao:"))
-        if (Aux != 1 and Aux != 2):
-            print("\nERRO!Opcao invalida!\n")
-    print("\t1 - AFD")
-    print("\t2 - AFN")
-    while (Aux2 != 1 and Aux2 != 2):
-        Aux2 = int(input("Entre com uma opcao:"))
-        if (Aux2 != 1 and Aux2 != 2):
-            print("\nERRO!Opcao invalida!\n")
-    if (Aux == 1):
-        Q = Arq.readline()
-        I = Arq.readline()
-        F = Arq.readline()
-        if Aux2 == 1:
-            Automato1 = Automato(Q, I, F)  # Instanciando objeto com respectivos estados
-        elif Aux2 == 2:
-            Automato1 = AutomatoAFN(Q, I, F)  # Instanciando objeto com respectivos estados
+            for linha in Arq:
+                if (linha == "---\n"):
+                    break
+                Automato1.AdicionaTransicao(linha)  # Adicionando transicoes
 
-        for linha in Arq:
-            if (linha == "---\n"):
-                break
-            Automato1.AdicionaTransicao(linha)  # Adicionando transicoes
+            for linha in Arq:
+                Automato1.AdicionaEntrada(linha)  # Adicionando Entradas
 
-        for linha in Arq:
-            Automato1.AdicionaEntrada(linha)  # Adicionando Entradas
+            print(Automato1.Estados)  # Prints de debug
+            print(Automato1.EstadoI)
+            print(Automato1.EstadosF)
+            print(Automato1.Origem)
+            print(Automato1.Destino)
+            print(Automato1.SimbolosEntrada)
+            print(Automato1.Entradas)
 
-        print(Automato1.Estados)  # Prints de debug
-        print(Automato1.EstadoI)
-        print(Automato1.EstadosF)
-        print(Automato1.Origem)
-        print(Automato1.Destino)
-        print(Automato1.SimbolosEntrada)
-        print(Automato1.Entradas)
+            Automato1.RealizaComputacao()
 
-        Automato1.RealizaComputacao()
+        elif (Aux == 2):
+            Q = Arq.readline()
+            S = Arq.readline()
+            I = Arq.readline()
+            F = Arq.readline()
+            if '\\' in S:
+                print('Não pode haver \\ no alfabeto')
+                print('Reiniciando o Programa...\n')
+                Arq.close()
+                main()
+                return
+            if Aux2 == 1:
+                # Instanciando objeto com respectivos estados
+                Automato1 = Automato(Q, I, F)
+            elif Aux2 == 2:
+                # Instanciando objeto com respectivos estados
+                Automato1 = AutomatoAFN(Q, I, F)
 
-    elif (Aux == 2):
-        Q = Arq.readline()
-        S = Arq.readline()
-        I = Arq.readline()
-        F = Arq.readline()
-        if Aux2 == 1:
-            # Instanciando objeto com respectivos estados
-            Automato1 = Automato(Q, I, F)
-        elif Aux2 == 2:
-            # Instanciando objeto com respectivos estados
-            Automato1 = AutomatoAFN(Q, I, F)
+            for linha in Arq:
+                if (linha == "---\n"):
+                    break
+                Automato1.AdicionaTransicao(linha)  # Adicionando transicoes
 
-        for linha in Arq:
-            if (linha == "---\n"):
-                break
-            Automato1.AdicionaTransicao(linha)  # Adicionando transicoes
+            for linha in Arq:
+                Automato1.AdicionaEntrada(linha)  # Adicionando Entradas
 
-        for linha in Arq:
-            Automato1.AdicionaEntrada(linha)  # Adicionando Entradas
+            print(Automato1.Estados)  # Prints de debug
+            print(Automato1.EstadoI)
+            print(Automato1.EstadosF)
+            print(Automato1.Origem)
+            print(Automato1.Destino)
+            print(Automato1.SimbolosEntrada)
+            print(Automato1.Entradas)
+            Automato1.RealizaComputacao()
+        Arq.close()
+        pass
 
-        print(Automato1.Estados)  # Prints de debug
-        print(Automato1.EstadoI)
-        print(Automato1.EstadosF)
-        print(Automato1.Origem)
-        print(Automato1.Destino)
-        print(Automato1.SimbolosEntrada)
-        print(Automato1.Entradas)
-
-        Automato1.RealizaComputacao()
-    Arq.close()
-    pass
+main()
